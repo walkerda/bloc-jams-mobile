ca510ce26aaa5bd16a2841f739a70bed95abd60f
Daniel Walker <walkerda2010@gmail.com>
2014-08-15 02:35:45 +0000

    Converted landing page to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,33 @@
-require("./landing");
-require("./collection");
-require("./album");
-require("./profile");
+//require("./landing");
+//require("./collection");
+//require("./album");
+//require("./profile");
+
+function shuffle(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+    $scope.headingText = "Bloc Jams";
+    $scope.headingTextClicked = function () {
+        shuffle($scope.albumURLs);
+    };
+
+    $scope.subText = "Get Down, Get Funky!";
+    $scope.subTextClicked = function() {
+        $scope.subText += '!';
+    };
+
+    $scope.albumURLs = [
+        '/images/album-placeholders/album-1.jpg',
+        '/images/album-placeholders/album-2.jpg',
+        '/images/album-placeholders/album-3.jpg',
+        '/images/album-placeholders/album-4.jpg',
+        '/images/album-placeholders/album-5.jpg',
+        '/images/album-placeholders/album-6.jpg',
+        '/images/album-placeholders/album-7.jpg',
+        '/images/album-placeholders/album-8.jpg',
+        '/images/album-placeholders/album-9.jpg'
+    ];
+}]);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -264,10 +264,39 @@ if (document.URL.match(/\/album.html/)) {
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing");
-require("./collection");
-require("./album");
-require("./profile");
+//require("./landing");
+//require("./collection");
+//require("./album");
+//require("./profile");
+
+function shuffle(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+    $scope.headingText = "Bloc Jams";
+    $scope.headingTextClicked = function () {
+        shuffle($scope.albumURLs);
+    };
+
+    $scope.subText = "Get Down, Get Funky!";
+    $scope.subTextClicked = function() {
+        $scope.subText += '!';
+    };
+
+    $scope.albumURLs = [
+        '/images/album-placeholders/album-1.jpg',
+        '/images/album-placeholders/album-2.jpg',
+        '/images/album-placeholders/album-3.jpg',
+        '/images/album-placeholders/album-4.jpg',
+        '/images/album-placeholders/album-5.jpg',
+        '/images/album-placeholders/album-6.jpg',
+        '/images/album-placeholders/album-7.jpg',
+        '/images/album-placeholders/album-8.jpg',
+        '/images/album-placeholders/album-9.jpg'
+    ];
+}]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
