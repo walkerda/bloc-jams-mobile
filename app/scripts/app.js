125b2f84c823a261aaa9272f3dc164338f22bcf9
Daniel Walker <walkerda2010@gmail.com>
2014-08-18 05:54:19 +0000

    Converted album view to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -45,6 +45,12 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
             controller: 'Collection.controller',
             templateUrl: '/templates/collection.html'
         })
+
+        .state('album', {
+            url: '/album',
+            controller: 'Album.controller',
+            templateUrl: '/templates/album.html'
+        })
 }]);
 
 // This is a cleaner way to call the controller than crowding it on the module definition.
@@ -77,5 +83,40 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
     for (var i = 0; i < 33; i++) {
         $scope.albums.push(angular.copy(albumPicasso));
     }
+}]);
+
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+    $scope.album = angular.copy(albumPicasso);
+
+    var hoveredSong = null;
+    var playingSong = null;
+
+    $scope.onHoverSong = function(song) {
+        hoveredSong = song;
+    };
+
+    $scope.offHoverSong = function(song) {
+        hoveredSong = null;
+    };
+
+    $scope.getSongState = function(song) {
+        if (song === playingSong) {
+            return 'playing';
+        }
+        else if (song === hoveredSong){
+            return 'hovered';
+        }
+        else {
+            return 'default';
+        }
+    };
+
+    $scope.playSong = function(song) {
+        playingSong = song;
+    };
+
+    $scope.pauseSong = function(song) {
+        playingSong = null;
+    };
 }]);
 
